979d20e3a56e10888525f1fd2760d98c7366d62f
Sharon Valeria <Sharon Valeria@DESKTOP-4AK6NKK>
2024-03-15 08:36:12 -0600

    primera carga

---
+++ src/main/java/ucr/ac/cr/programacion/ii/ProgramacionII.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.programacion.ii;
+
+/**
+ *
+ * @author Sharon Valeria
+ */
+public class ProgramacionII {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
